2f00ae04dbf0ca5355c8bd5d804a0abec3067bfc
ardisaurus <ardinisme@gmail.com>
2018-03-14 11:54:29 -0400

    Initial commit

---
+++ application/controllers/User.php
@@ -0,0 +1,103 @@
+<?php
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class User extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+    }
+
+    function index_get() {            
+        $email = $this->get('email');
+        if ($email == '') {
+            $this->db->select('email');
+            $this->db->select('name');
+            $this->db->select('dob'); 
+            $this->db->select('level'); 
+            $this->db->select('active');            
+            $this->db->order_by("level", "asc");
+            $user = $this->db->get('user')->result();
+        } else {
+            $this->db->select('email');
+            $this->db->select('name');
+            $this->db->select('dob'); 
+            $this->db->select('level'); 
+            $this->db->select('active');
+            $this->db->where('email', $email);
+            $user = $this->db->get('user')->result();
+        }
+        $this->response($user, 200);
+    }
+
+    function index_post() {
+        $action=$this->post('action');        
+        if ($action=="insert") {
+            $data = array(
+                    'email'     => $this->post('email'),
+                    'name'      => $this->post('name'),
+                    'password'  => $this->post('password'),
+                    'level'     => $this->post('level'),
+                    'dob'       => $this->post('dob'));
+            $insert = $this->db->insert('user', $data);
+            if ($insert) {
+                $this->response($data, 200);
+            } else {
+                $this->response(array('status' => 'fail', 502));
+            }
+        }elseif ($action=="auth") {
+            $email = $this->post('email');
+            $password = $this->post('password');
+            $this->db->where('email', $email);
+            $user = $this->db->get('user')->result();
+            if ($user[0]->password==$password ) {
+                $data[0] = array('status' => "success");
+                $this->response($data, 200);
+            } else {
+                $data[0] = array('status' => "fail");
+                $this->response($data, 200);
+            }  
+        }elseif ($action=="delete") {
+            $email = $this->post('email');
+            $this->db->where('email', $email);
+            $delete = $this->db->delete('user');
+            if ($delete) {
+                $this->response(array('status' => 'success'), 201);
+            } else {
+                $this->response(array('status' => 'fail', 502));
+            }
+        }elseif ($action=="update") {
+            $email = $this->post('email');
+            $part = $this->post('part');
+            if ($part=="email") {
+                $data = array('email' => $this->post('new_email'));
+            }elseif ($part=="name") {
+                $data = array('name' => $this->post('name'));
+            }elseif ($part=="password") {
+                $data = array('password' => $this->post('password'));
+            }elseif ($part=="dob") {
+                $data = array('dob' => $this->post('dob'));
+            }else{            
+                $this->response(array('status' => 'fail', 502));
+            }
+            $this->db->where('email', $email);
+            $update = $this->db->update('user', $data);
+            if ($update) {
+                $this->response($data, 200);
+            } else {
+                $this->response(array('status' => 'fail', 502));
+            }
+        }else{
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    function index_put() {
+        $this->response(array('status' => 'fail', 502));
+    }
+
+    function index_delete() {
+        $this->response(array('status' => 'fail', 502));
+    }
+
+}
